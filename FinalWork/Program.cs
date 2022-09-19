24c06abb043feeed52386e17b27ad02960f72419
Kirill_s <skorkin.kirill@gmail.com>
2022-09-19 20:56:06 +0300

    написал программу

---
+++ FinalWork/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых
+// меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на
+// старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+//исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.Clear();
+string[] stringArray = new string[4] { "Hello", ":)", "три", "четыре" };
+string[] miniArray = new string[stringArray.Length];
+int letter_counter = 0;
+int size = 3;
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= size)
+    {
+        miniArray[letter_counter] = stringArray[i];
+        letter_counter++;
+    }
+}
+Console.Write($"[{String.Join(" ", stringArray)}] -> ");
+Console.Write($"[{String.Join(" ", miniArray)}]");
